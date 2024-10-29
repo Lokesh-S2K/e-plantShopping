5a6726e063f7d2e55cff183e12e7a5aa755aeb91
Lokesh-S2K <lokesh2210200@ssn.edu.in>
2024-10-30 00:40:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import App from './App.jsx'; // Import your main App component
+import './index.css'; // Import your global CSS styles
+import { Provider } from 'react-redux'; // Import the Provider from react-redux
+import store from './store.js'; // Import the configured Redux store
+
+// Create the root and render the application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
+    <Provider store={store}> {/* Wrap your App component with Provider */}
+      <App />
     </Provider>
   </React.StrictMode>,
-)
+);
+
